67cadd9474df22339ebb4fdeadd954778e7b708c
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-24 21:15:46 -0300

    Connect with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 require("dotenv").config(); // Load environment variables
+require("colors"); // Pretty console logs
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
 const { errorHandler } = require("./middlewares/errorMiddleware");
-
-// Routes
 const goalRoutes = require("./routes/goalRoutes");
+const connectDB = require("./config/db");
+
+// Start database connection
+connectDB();
 
 // Middlewares
 app.use(express.json()); // Parse JSON Requests
