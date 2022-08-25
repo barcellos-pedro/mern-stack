3483a3b0506f89b5db22f261ae86da1d4f759ca3
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-25 15:41:01 -0300

    Register User method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const UserlModel = require("../models/userModel");
 
@@ -7,7 +9,42 @@ const UserlModel = require("../models/userModel");
  * @access Public
  */
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  // Find user by email and check if exists
+  const userExists = await UserlModel.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await UserlModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
+
+  res.status(201).json({
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+  });
 });
 
 /**
