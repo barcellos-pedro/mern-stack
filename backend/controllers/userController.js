d13af22ca6ba485de1d2c346d04dfa85b711964e
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-25 16:11:43 -0300

    Login User method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,27 @@ const registerUser = asyncHandler(async (req, res) => {
  * @access Public
  */
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Logged In" });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill all fields");
+  }
+
+  const user = await UserlModel.findOne({ email });
+
+  console.log(password, user);
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 /**
