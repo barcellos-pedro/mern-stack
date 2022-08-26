d443d9f0039d0ded509417e16bfba1defd05b83b
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-26 11:44:40 -0300

    Refactor auth middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -10,33 +10,39 @@ const protect = asyncHandler(async (req, res, next) => {
   let token;
   const { authorization } = req.headers;
 
+  // Check if authorization header is empty
   if (!authorization) {
     res.status(401);
     throw new Error("Not authorized, no token");
   }
 
-  // Chech if there is data in authorization header to get the token
-  if (authorization.startsWith("Bearer")) {
-    try {
-      // Get token from header (Bearer <token>)
-      token = authorization.split(" ").at(-1);
+  // Check if authorization header is an Bearer Token
+  if (!authorization.startsWith("Bearer")) {
+    res.status(401);
+    throw new Error("Invalid token format");
+  }
 
-      // Verify token
-      const decoded = jwt.verify(token, SECRET);
+  // Try to get token and verify it
+  try {
+    // Get token from header (Bearer <token>)
+    token = authorization.split(" ").at(-1);
 
-      // Get User by Id
-      // from the decoded token and assign to Request object
-      // Remove the password field from the query
-      req.user = await UserModel.findById(decoded.id).select("-password");
+    // Verify token
+    const decoded = jwt.verify(token, SECRET);
 
-      // Continue request/response lifecycle
-      // calling next middleware
-      next();
-    } catch (error) {
-      console.log(error);
-      res.status(401);
-      throw new Error("Not authorized");
-    }
+    // Get User by Id
+    // from the decoded token and assign to Request object
+    // Remove the password field from the query
+    req.user = await UserModel.findById(decoded.id).select("-password");
+
+    // Continue request/response lifecycle calling next middleware
+    // with User data inside request object
+    // so we can use User data inside private routes
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401);
+    throw new Error("Not authorized");
   }
 });
 
