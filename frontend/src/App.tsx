0a829c0e8579ac1ed28d3b94e5e23dc97f23ce38
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-29 21:10:48 -0300

    Add toast container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dis/ReactToastify.css';
 
+import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
