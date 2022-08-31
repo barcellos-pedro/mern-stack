fc6d4893443c9bf9e0ce8d603ec91a4123afb7cd
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-31 15:24:10 -0300

    Add login thunk

--- frontend/src/features/auth/authThunks.ts
+++ frontend/src/features/auth/authThunks.ts
@@ -1,10 +1,16 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { Login } from '../../types/Login';
 
 import { UserDTO } from '../../types/UserDTO';
 import authService from './authService';
 
+const handleError = (thunkAPI: any, error: any, fallbackMsg: string) => {
+  const message = error?.response?.data?.message || fallbackMsg;
+  return thunkAPI.rejectWithValue(message);
+};
+
 /**
- * Register User Async Thunk/Function
+ * Register User
  */
 export const register = createAsyncThunk(
   'auth/register',
@@ -16,10 +22,33 @@ export const register = createAsyncThunk(
       }
       return data;
     } catch (error: any) {
-      const message =
-        error?.response?.data?.message ||
-        'Error when creating the user. Try again.';
-      return thunkAPI.rejectWithValue(message);
+      return handleError(
+        thunkAPI,
+        error,
+        'Error to create your account. Please, try again'
+      );
+    }
+  }
+);
+
+/**
+ * Login User
+ */
+export const login = createAsyncThunk(
+  'auth/login',
+  async (formData: Login, thunkAPI) => {
+    try {
+      const { data } = await authService.login(formData);
+      if (data) {
+        localStorage.setItem('user', JSON.stringify(data));
+      }
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error: any) {
+      return handleError(
+        thunkAPI,
+        error,
+        'Error during login. Please, try again.'
+      );
     }
   }
 );
