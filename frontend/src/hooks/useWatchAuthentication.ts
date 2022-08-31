c0ce8eea6a716919f1e1e398f51e2877a04c4ff2
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-31 16:43:09 -0300

    Create authentication hook

---
+++ frontend/src/hooks/useWatchAuthentication.ts
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { reset } from '../features/auth/authSlice';
+import { AuthState } from '../types/AuthState';
+
+/**
+ * Watch authentication flow.
+ *
+ * On User Login/Register
+ */
+export function useWatchAuthentication() {
+  // Use to navigate to other routes
+  const navigate = useNavigate();
+
+  // Dispatch store actions
+  const dispatch = useDispatch<any>();
+  // Grab auth state fields
+  const { user, loading, error, message } = useSelector(
+    (state: any) => state.auth as AuthState
+  );
+
+  useEffect(() => {
+    if (error) {
+      toast.error(message);
+    }
+
+    if (user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [error, message, user, dispatch, navigate]);
+
+  return { loading, dispatch };
+}
