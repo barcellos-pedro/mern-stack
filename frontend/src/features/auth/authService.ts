72d32701e71c9de796d3d739d871d17cd1d699b0
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-31 10:12:45 -0300

    Add type

--- frontend/src/features/auth/authService.ts
+++ frontend/src/features/auth/authService.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import { UserDTO } from '../../types/UserDTO';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:5000/api/users',
 });
 
 const authService = {
-  register(user: any) {
-    return apiClient.post('/', user);
+  register(user: UserDTO) {
+    return apiClient.post<UserDTO>('/', user);
   },
 };
 
