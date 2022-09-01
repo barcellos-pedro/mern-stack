07fcfd22578ea0a6c256c84643e331223a09b44f
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-01 12:25:14 -0300

    Add create goal thunk

--- frontend/src/features/goals/goalsThunks.ts
+++ frontend/src/features/goals/goalsThunks.ts
@@ -1,16 +1,27 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { RootState } from '../../app/store';
 import goalsService from './goalsService';
+
+// Helpers
+const NOT_AUTHORIZED_ERROR = 'Not authorized';
+
+const getToken = (thunkAPI: any): string | undefined => {
+  const rootState = thunkAPI.getState() as RootState;
+  return rootState.auth.user?.token;
+};
 
 const handleError = (thunkAPI: any, error: any, fallbackMsg: string) => {
   const message = error?.response?.data?.message || fallbackMsg;
   return thunkAPI.rejectWithValue(message);
 };
 
+// Async actions
 export const getGoals = createAsyncThunk(
   'goals/getGoals',
   async (token: string, thunkAPI) => {
     if (!token) {
-      throw new Error('Not authorized');
+      throw new Error(NOT_AUTHORIZED_ERROR);
     }
     try {
       const { data } = await goalsService.getGoals(token);
@@ -20,6 +31,27 @@ export const getGoals = createAsyncThunk(
         thunkAPI,
         error,
         'Error loading Goals. Please try again later.'
+      );
+    }
+  }
+);
+
+export const createGoal = createAsyncThunk(
+  'goals/createGoal',
+  async (payload: { text: string }, thunkAPI) => {
+    const token = getToken(thunkAPI);
+    if (!token) {
+      throw new Error(NOT_AUTHORIZED_ERROR);
+    }
+
+    try {
+      const { data } = await goalsService.createGoal(payload, token);
+      return data;
+    } catch (error) {
+      return handleError(
+        thunkAPI,
+        error,
+        'Error to create Goal. Please try again later.'
       );
     }
   }
