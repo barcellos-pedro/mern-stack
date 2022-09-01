929e9f6c798171460855f2c52cca0e9e33356478
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-01 12:24:06 -0300

    Implement Create Goal

--- frontend/src/components/GoalsForm.tsx
+++ frontend/src/components/GoalsForm.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+
+import { AppDispatch, RootState } from '../app/store';
+import { reset } from '../features/goals/goalsSlice';
+import { createGoal } from '../features/goals/goalsThunks';
+
 function GoalsForm() {
+  const [text, setText] = useState<string>('');
+  const dispatch = useDispatch<AppDispatch>();
+  const goalsState = useSelector((state: RootState) => state.goals);
+  const { error, message } = goalsState;
+
+  const onSubmitForm = (e: any) => {
+    e.preventDefault();
+    dispatch(createGoal({ text }));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(message);
+    }
+
+    dispatch(reset());
+  }, [error, message, dispatch]);
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={onSubmitForm}>
       <div className="form-group">
         <label htmlFor="text">Goal</label>
-        <input type="text" name="text" id="text" required={true} />
+        <input
+          id="text"
+          name="text"
+          type="text"
+          placeholder="Learn something new! 💡"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required={true}
+        />
       </div>
 
       <button className="btn btn-block" type="submit">
