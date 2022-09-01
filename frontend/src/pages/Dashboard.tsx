d1add682f5e8a3ab789242d9ae396fe4868062fc
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-31 22:38:28 -0300

    List Goals on Dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,21 +1,48 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { AuthState } from '../types/AuthState';
+import { toast } from 'react-toastify';
+import { AppDispatch, RootState } from '../app/store';
+import GoalsList from '../components/GoalsList';
+
+import Spinner from '../components/Spinner';
+import { reset } from '../features/goals/goalsSlice';
+import { getGoals } from '../features/goals/goalsThunks';
 
 function Dashboard() {
   const navigate = useNavigate();
-  const { user } = useSelector((state: any) => state.auth as AuthState);
+  const dispatch = useDispatch<AppDispatch>();
+  const rootState = useSelector((state: RootState) => state);
+  const { user } = rootState.auth;
+  const { goals, error, loading, message } = rootState.goals;
 
   useEffect(() => {
     if (!user) {
       navigate('/login');
     }
-  }, [navigate, user]);
+
+    if (user) {
+      dispatch(getGoals(user.token));
+    }
+
+    if (error) {
+      toast.error(message);
+    }
+
+    dispatch(reset());
+  }, [navigate, user, dispatch, error, message]);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <div>
       <h1>Dashboard</h1>
+      <h2>Welcome {user && `${user.name}`}</h2>
+      <section className="content">
+        <GoalsList list={goals} />
+      </section>
     </div>
   );
 }
