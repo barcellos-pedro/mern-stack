de130ee8d7281d48123a7a683d0f93541be74509
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-31 22:30:34 -0300

    Add goals reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import goalsReducer from '../features/goals/goalsSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    goals: goalsReducer,
   },
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
