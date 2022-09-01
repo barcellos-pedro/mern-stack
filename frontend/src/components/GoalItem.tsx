2d81d74abb38c6a2760085a6bcba94fb10b2ec23
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-01 16:44:40 -0300

    Implement delete goal

--- frontend/src/components/GoalItem.tsx
+++ frontend/src/components/GoalItem.tsx
@@ -1,4 +1,10 @@
 // import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { AppDispatch, RootState } from '../app/store';
+import { reset } from '../features/goals/goalsSlice';
+import { deleteGoal } from '../features/goals/goalsThunks';
 import { Goal } from '../types/Goal';
 
 type GoalItemProps = {
@@ -6,19 +12,30 @@ type GoalItemProps = {
 };
 
 function GoalItem({ goal }: GoalItemProps) {
-  // const dispatch = useDispatch<any>();
+  const dispatch = useDispatch<AppDispatch>();
+  const goalsState = useSelector((state: RootState) => state.goals);
+  const { error, message } = goalsState;
+
+  // Helper to show parsed date
   const getGoalDate = ({ createdAt }: Goal) =>
     new Date(createdAt).toLocaleString();
+
+  useEffect(() => {
+    if (error) {
+      toast.error(message);
+    }
+
+    dispatch(reset());
+  }, [error, message, dispatch]);
 
   return (
     <div className="goal">
       <div>{getGoalDate(goal)}</div>
       <h2>{goal.text}</h2>
-      {/* TODO: Delete Goal */}
       <button
         title="Delete Goal"
         className="close"
-        onClick={() => console.log('delete goal')}
+        onClick={() => dispatch(deleteGoal(goal._id))}
       >
         X
       </button>
